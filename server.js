9e93a3b2b9de4c96d05057dc8d37ff276ae44d84
Josh Hawks <snl95@txstate.edu>
2022-10-16 14:40:47 -0500

    server/ db setup

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const session = require('express-session');
+const hbrs = require('express-handlebars');
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+	secret: 'Super secret secret',
+	cookie: {},
+	resave: false,
+	saveUninitialized: true,
+	store: new SequelizeStore({
+		db: sequelize.db,
+	}),
+};
+
+app.use(session(sess));
+
+const hbs = hbrs.create({ helpers });
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(require('./controllers/'));
+
+sequelize.sync({ force: false }).then(() => {
+	app.listen(PORT, () => console.log('Now listening'));
+});
