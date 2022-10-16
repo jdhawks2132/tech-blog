bf210047715de429b61344da10414dcbc3417c00
Josh Hawks <snl95@txstate.edu>
2022-10-16 15:16:49 -0500

    models updated

---
+++ models/Post.js
@@ -0,0 +1,32 @@
+const { Mode, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection');
+
+class Post extends Model {}
+
+Post.init(
+	{
+		id: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			primaryKey: true,
+			autoIncrement: true,
+		},
+		title: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		body: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+	},
+	{
+		sequelize,
+		modelName: 'post',
+		freezeTableName: true,
+		underscored: true,
+	}
+);
+
+module.exports = Post;
--- server.js
+++ server.js
@@ -7,19 +7,19 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 
 const sequelize = require('./config/connection');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+// const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-const sess = {
-	secret: 'Super secret secret',
-	cookie: {},
-	resave: false,
-	saveUninitialized: true,
-	store: new SequelizeStore({
-		db: sequelize.db,
-	}),
-};
+// const sess = {
+// 	secret: 'Super secret secret',
+// 	cookie: {},
+// 	resave: false,
+// 	saveUninitialized: true,
+// 	store: new SequelizeStore({
+// 		db: sequelize.db,
+// 	}),
+// };
 
-app.use(session(sess));
+// app.use(session(sess));
 
 const hbs = hbrs.create({});
 
@@ -30,8 +30,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(require('./controllers/'));
+// app.use(require('./controllers/'));
 
-sequelize.sync({ force: false }).then(() => {
+sequelize.sync({ force: true }).then(() => {
 	app.listen(PORT, () => console.log('Now listening'));
 });
